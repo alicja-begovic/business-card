5574d1180d4b2eae02317dcda08d4c38b06cb348
Alicja Begović <alicja.begovic@gmail.com>
2022-05-29 17:26:49 +0200

    js button and added day/naight mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../scss/main.scss';
 
 console.log('HELLO 🚀')
+
+let isDark = false;
+
+const switchModes = document.querySelector('.mode--js');
+
+switchModes.addEventListener('click', () => {
+    if (isDark) {
+        document.documentElement.style.setProperty('--background-color', '#353a47');
+        document.documentElement.style.setProperty('--text-color', '#9b384e');
+        isDark = false;
+    } else {
+        document.documentElement.style.setProperty('--background-color', '#9b384e');
+        document.documentElement.style.setProperty('--text-color', '#353a47');
+        isDark = true;
+    }
+})
