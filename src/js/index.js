fcfbc5c1c02d510ab883c417fcbbd3151c228209
Alicja Begović <alicja.begovic@gmail.com>
2022-07-03 16:07:21 +0200

    js changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,17 @@
-import '../scss/main.scss';
-
-console.log('HELLO 🚀')
+import "../scss/main.scss";
 
 let isDark = false;
 
-const switchModes = document.querySelector('.mode--js');
+const switchModes = document.querySelector(".mode--js");
 
-switchModes.addEventListener('click', () => {
-    if (isDark) {
-        document.documentElement.style.setProperty('--background-color', '#353a47');
-        document.documentElement.style.setProperty('--text-color', '#9b384e');
-        isDark = false;
-    } else {
-        document.documentElement.style.setProperty('--background-color', '#9b384e');
-        document.documentElement.style.setProperty('--text-color', '#353a47');
-        isDark = true;
-    }
-})
+switchModes.addEventListener("click", () => {
+  if (isDark) {
+    document.documentElement.style.setProperty("--background-color", "#353a47");
+    document.documentElement.style.setProperty("--text-color", "#9b384e");
+    isDark = false;
+  } else {
+    document.documentElement.style.setProperty("--background-color", "#9b384e");
+    document.documentElement.style.setProperty("--text-color", "#353a47");
+    isDark = true;
+  }
+});
